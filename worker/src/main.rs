9adbe0b551ce2d54de4afa269e185283fba848f0
WilliamQiufeng <williamqiufeng@outlook.com>
2026-08-21 21:32:53 +0100

    Test shared memory setup

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -1,3 +1,8 @@
+use std::{env::args, fs::File};
+
+use anyhow::bail;
+use lib::shared_memory;
+use std::io::Write;
 
 pub fn apply_sandbox() -> anyhow::Result<()> {
     #[cfg(target_os = "linux")]
@@ -10,8 +15,19 @@ pub fn apply_sandbox() -> anyhow::Result<()> {
 }
 
 fn main() -> anyhow::Result<()> {
+    let args = args().collect::<Vec<_>>();
+    if args.len() < 3 {
+        bail!("Invalid argument");
+    }
+    let path = &args[1];
+    let size = usize::from_str_radix(&args[2], 10)?;
+    let file = File::open(path)?;
+
+    let mut mem = shared_memory::SharedMemoryInstance::from_file(&file, size)?;
+
     apply_sandbox()?;
     lib::test()?;
+    writeln!(mem, "Hi")?;
     println!("Hello, world!");
     Ok(())
 }
